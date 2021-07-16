c94836ee6c3c62f5bac4085c2807cbeeebfc6225
warren yun <xxwarrenhdxx@gmail.com>
2021-07-15 21:10:13 -0400

    Make info route

--- server/main.py
+++ server/main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+@app.route("/info", methods=['POST'])
+def info():
+    print(request.json)
+    return jsonify({"test": "yes"})
 
 if __name__ == '__main__':
     app.run(debug=True)
